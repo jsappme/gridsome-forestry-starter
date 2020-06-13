74f306a037139d9aec42e9daeb3080560e3337fc
JSapp.me <contact@jsapp.me>
2020-06-13 10:53:32 -0600

    title chamge

--- gridsome.config.js
+++ gridsome.config.js
@@ -2,9 +2,9 @@
 // Learn more: https://gridsome.org/docs/config
 
 module.exports = {
-  siteName: "Portfolio",
-  siteUrl: `https://www.itsnwa.com`,
-  host: "0.0.0.0",
+  siteName: "Gridsomify!",
+  siteUrl: `https://gridsomify.com`,
+  //host: "0.0.0.0",
   titleTemplate: "%s - NWA",
   siteDescription: "Creative technologist",
   plugins: [
