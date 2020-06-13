2e06a6ec8f04d39c2887b5ecd5e689ab641acd1c
JSapp.me <contact@jsapp.me>
2020-06-13 12:20:03 -0600

    main

--- src/main.js
+++ src/main.js
@@ -3,9 +3,59 @@ import settings from "../data/theme.json";
 
 import "@/assets/code-highlight.css"
 
+// Plugins
+import Vuex from 'vuex'
+import VueApollo from 'vue-apollo'
+import Notifications from 'vue-notification/dist/ssr.js'
+
+// Dependencies
+import ApolloClient from 'apollo-boost'
+import fetch from 'isomorphic-fetch'
+
 export default function(Vue, { head }) {
   Vue.component("Layout", DefaultLayout);
   head.bodyAttrs = {
     class: settings.dark_mode ? "dark" : ""
   };
+
+  // Import global plugins
+  Vue.use(Vuex)
+  Vue.use(VueApollo)
+  Vue.use(Notifications)
+
+  // Create Apollo client
+  const apolloClient = new ApolloClient({
+    fetch,
+    uri: `https://${process.env.GRIDSOME_SHOPIFY_STOREFRONT}.myshopify.com/api/2019-07/graphql.json`,
+    headers: {
+      'X-Shopify-Storefront-Access-Token': process.env.GRIDSOME_SHOPIFY_STOREFRONT_TOKEN
+    }
+  })
+
+  // Add client to vue-apollo provider
+  const apolloProvider = new VueApollo({
+    defaultClient: apolloClient
+  })
+
+  // Add provider to vue app
+  appOptions.apolloProvider = apolloProvider
+
+  // Create Vuex store
+  appOptions.store = new Vuex.Store({
+    state: {
+      cart: []
+    },
+    mutations: {
+      addToCart: (state, newItem) => {
+        const itemExists = state.cart.find(item => item.variantId === newItem.variantId)
+
+        if (itemExists) itemExists.qty += newItem.qty
+        else state.cart.push(newItem)
+      },
+      removeFromCart: (state, itemId) => {
+        const updatedCart = state.cart.filter(item => item.variantId !== itemId)
+        state.cart = updatedCart
+      }
+    }
+  })
 }
