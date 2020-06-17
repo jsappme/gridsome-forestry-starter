7b12ceb90f4dfb59e732e1d2fd4f76a820375fab
JSapp.me <contact@jsapp.me>
2020-06-17 11:41:31 -0600

    main

--- src/main.js
+++ src/main.js
@@ -1,36 +1,70 @@
-// This is the main.js file. Import global CSS and scripts here.
-// The Client API can be used here. Learn more: gridsome.org/docs/client-api
+// Plugins
+import Vuex from 'vuex'
+import VueApollo from 'vue-apollo'
+
+//import Notifications from 'vue-notification'
+import Notifications from 'vue-notification/dist/ssr'
+import '~/assets/style/notification.css'
+
+// Dependencies
+import ApolloClient from 'apollo-boost'
+import fetch from 'isomorphic-fetch'
 
 import DefaultLayout from '~/layouts/Default.vue'
 import VueScrollTo from 'vue-scrollto'
 
-export default function (Vue, { router, head, isClient }) {
+export default function (Vue, { appOptions, router, head, isClient }) {
   // Set default layout as a global component
   Vue.component('Layout', DefaultLayout)
+
+  Vue.use(Vuex)
+  Vue.use(VueApollo)
+  Vue.use(Notifications)
 
   Vue.use(VueScrollTo, {
     duration: 500,
     easing: "ease",
   })
 
-  head.meta.push({
-    name: 'keywords',
-    content: 'algo,crypto,trading,binance,autotrading,trading bots,machine learning,automl,coder trader,binance api,backtesting,forward testing'
-  })
-
-  head.meta.push({
-    name: 'description',
-    content: 'Algo Crypto Trading'
-  })
-
-  head.meta.push({
-    name: 'author',
-    content: 'Herve Fulchiron'
-  })
-
   head.link.push({
     rel: 'stylesheet',
     href: 'https://fonts.googleapis.com/css?family=Prata'
+  })
+
+  // Create Apollo client
+  const apolloClient = new ApolloClient({
+    fetch,
+    uri: `https://${process.env.GRIDSOME_SHOPIFY_STOREFRONT}.myshopify.com/api/2019-07/graphql.json`,
+    headers: {
+      'X-Shopify-Storefront-Access-Token': process.env.GRIDSOME_SHOPIFY_STOREFRONT_TOKEN
+    }
+  })
+
+  // Add client to vue-apollo provider
+  const apolloProvider = new VueApollo({
+    defaultClient: apolloClient
+  })
+
+  // Add provider to vue app
+  appOptions.apolloProvider = apolloProvider
+
+  // Create Vuex store
+  appOptions.store = new Vuex.Store({
+    state: {
+      cart: []
+    },
+    mutations: {
+      addToCart: (state, newItem) => {
+        const itemExists = state.cart.find(item => item.variantId === newItem.variantId)
+
+        if (itemExists) itemExists.qty += newItem.qty
+        else state.cart.push(newItem)
+      },
+      removeFromCart: (state, itemId) => {
+        const updatedCart = state.cart.filter(item => item.variantId !== itemId)
+        state.cart = updatedCart
+      }
+    }
   })
 }
 
