1d10d43a035ea6c1bb63f2e3acd26785a5da6df0
JSapp.me <contact@jsapp.me>
2020-06-17 15:47:48 -0600

    cart etc

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -8,7 +8,7 @@
     </div>
 
     <div id="products" class="flex flex-wrap mb-4 max-w-5xl mx-auto">
-			<div class="md:w-1/2 lg:w-1/3 w-full px-2" v-for="({ node: product }) in featuredProducts">
+			<div class="md:w-1/2 lg:w-1/3 w-full px-2" v-on:click="selectProduct({ node: product })" v-for="({ node: product }) in featuredProducts">
         <div class="mx-auto max-w-sm overflow-hiddenmx-auto cursor-pointer duration-500 transform hover:scale-110">
           <div class="w-full rounded-xl h-64 bg-no-repeat bg-contain bg-bottom" :style="{'background-image': 'url(' + product.images[0].src + ')' }"></div>
             <div class="px-6 pb-4 pt-8 mx-4 -mt-4 mb-4 relative rounded-xl bg-white opacity-75 hover:opacity-100">
@@ -38,6 +38,12 @@ export default {
   computed: {
     collection () { return this.$page.allShopifyCollection.edges.length && this.$page.allShopifyCollection.edges[ 0 ].node },
     featuredProducts () { return this.$page.allShopifyProduct.edges }
+  },
+  methods: {
+		selectProduct(product) {
+      console.log("/product/" + product.node.handle)
+      this.$router.push({ path: "/product/" + product.node.handle })
+    },
   }
 }
 </script>
